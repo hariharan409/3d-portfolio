1fcb9f4da693259e85a2b28785d4d0f021e3b044
hariharan <hharan987@gmail.com>
2025-02-10 21:42:38 +0800

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import moment from "moment";
+
+const Hero = () => {
+  const careerStartedDate = moment(import.meta.env.VITE_CAREER_STARTED_DATE,"DD-MM-YYYY");
+  const currentDate = moment(new Date());
+
+  const monthsDiff = currentDate.diff(careerStartedDate,"months");
+  // Convert months to years with one decimal place
+  const yearsDiff = (monthsDiff / 12).toFixed(1);
+
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Hari</span>
+          </h1>
+          <h5 className="text-xl tracking-wider">{yearsDiff} years of experience</h5>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            <span className="text-[#915EFF]">Full-Stack Engineer at Keppel, Singapore</span>
+            <span className="xs:hidden md:inline">, passionate about building scalable web and mobile applications.Currently pursuing an MTech in Software Engineering (long-distance) at BITS Pilani.</span>
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
