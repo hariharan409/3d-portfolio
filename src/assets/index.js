5a920e0fc33e35ae26f900105e4fe6bddd0cbf94
hariharan <hharan987@gmail.com>
2025-02-11 20:00:20 +0800

    update experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,5 @@
 import hari from "./hari-pic.jpg";
 import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
@@ -22,21 +19,16 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+/* List of Companies I Have Worked With (Including My Current Role) */
 import keppel from "./company/keppel.png";
+import inforge from "./company/inforge.jpg";
+import fiveSixtyDegree from "./company/560-degree.png";
 
 export {
   logo,
-  backend,
-  creator,
-  mobile,
   web,
   github,
   menu,
@@ -54,13 +46,11 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
   hari,
-  keppel
+  keppel,
+  inforge,
+  fiveSixtyDegree
 };
