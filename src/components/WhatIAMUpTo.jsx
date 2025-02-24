8f97baa3e1b2e7594499527fddd163d449d20065
hariharan <hharan987@gmail.com>
2025-02-24 10:47:35 +0800

    add drawer max height

--- src/components/WhatIAMUpTo.jsx
+++ src/components/WhatIAMUpTo.jsx
@@ -15,11 +15,11 @@ const WhatIAMUpTo = () => {
 
             {/* Drawer Component */}
             <div
-                className={`fixed bottom-0 left-0 right-0 z-40 w-full p-4 transition-transform bg-black-200 text-white-100 ${ isOpen ? "translate-y-0" : "translate-y-full"}`}>
+                className={`fixed bottom-0 left-0 right-0 z-50 w-full p-4 transition-transform bg-black-200 text-white-100 ${ isOpen ? "translate-y-0" : "translate-y-full"}`}>
                 <h5 className="inline-flex items-center mb-4 text-base font-semibold text-[#915EFF]">
                     "Ongoing Projects" / "Learning & Exploring"
                 </h5>
-                <ul className="list-disc marker:text-[#915EFF] pl-5">
+                <ul className="list-disc marker:text-[#915EFF] pl-5 max-h-[300px] overflow-y-auto">
                     <li>
                         Developing Harvest Hub App – Building an app for farmers to manage their expenses and workflow efficiently. Enhancing AI capabilities to provide smart prompts and assist with decision-making.
                     </li>
